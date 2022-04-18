1fd116efacb0341ed9d8f65d11254a41d81310c9
王锋 <822690543@qq.com>
2022-04-18 21:24:20 +0800

    优化

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,15 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import routes from 'virtual:generated-pages'
-import { getToken } from '../utils/auth'
+import routes from "virtual:generated-pages";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 
 routes.push({
-  path: '/',
-  redirect: '/login',
-})
+  path: "/",
+  redirect: "/login",
+});
 
-console.log(routes, '自动生成的路由')
+console.log(routes, "自动生成的路由");
 //导入生成的路由数据
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
-})
-export default router
+});
+export default router;
