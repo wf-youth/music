5aeee37031e3e55785323640727c582caaced676
曹贺影 <15155868741@163.com>
2022-08-03 11:22:56 +0800

    底部

--- src/components/Layout/components/layout-footer/index.vue
+++ src/components/Layout/components/layout-footer/index.vue
@@ -27,10 +27,57 @@
           </div>
         </div>
       </div>
-      <div>
-        <like theme="outline" size="28" fill="#333" :strokeWidth="1" />
+      <div class="flex justify-between items-center w-44">
+        <like
+          :theme="isLike ? 'filled' : 'outline'"
+          size="22"
+          :fill="isLike ? ['#c24437'] : ['#333']"
+          :strokeWidth="1"
+          @click="handleLike"
+        />
+        <go-start
+          theme="outline"
+          size="22"
+          :fill="['#c24437', '#c24437', '#FFF', '#fff']"
+          :strokeWidth="1"
+        />
+        <play
+          v-if="isPlay"
+          theme="multi-color"
+          size="33"
+          :fill="['#c24437', '#c24437', '#FFF', '#fff']"
+          :strokeWidth="1"
+          @click="handlePlay"
+        />
+        <pause-one
+          v-else
+          theme="multi-color"
+          size="33"
+          :fill="['#c24437', '#c24437', '#FFF', '#fff']"
+          :strokeWidth="1"
+          @click="handlePlay"
+        />
+        <go-end
+          theme="outline"
+          size="22"
+          :fill="['#c24437', '#c24437', '#FFF', '#fff']"
+          :strokeWidth="1"
+        />
+        <delete theme="outline" size="22" fill="#333" :strokeWidth="1" />
       </div>
-      <div>声音操作</div>
+      <div class="flex justify-between items-center w-20">
+        <monitor-two theme="outline" size="17" fill="#333" :strokeWidth="1" />
+        <div
+          :style="{
+            color: is_show_word ? '#c24437' : '#000',
+            userSelect: 'none',
+          }"
+          @click="handleWord"
+        >
+          词
+        </div>
+        <volume-notice theme="outline" size="17" fill="#333" :strokeWidth="1" />
+      </div>
     </div>
     <!-- 哎嘿嘿 这是底部 -->
   </div>
@@ -38,7 +85,44 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
-import { Like } from "@icon-park/vue-next";
+import {
+  Like,
+  GoEnd,
+  GoStart,
+  Play,
+  PauseOne,
+  Delete,
+  MonitorTwo,
+  VolumeNotice,
+} from "@icon-park/vue-next";
+
+// 是否播放
+let isPlay = $ref(true);
+// 修改播放暂停
+function handlePlay() {
+  isPlay = !isPlay;
+}
+// 是否喜欢
+let isLike = $ref(false);
+// 修改喜欢/不喜欢
+function handleLike() {
+  isLike = !isLike;
+}
+
+// 是否显示词
+let is_show_word = $ref(false);
+// 点击歌词显示与否
+function handleWord() {
+  is_show_word = !is_show_word;
+}
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.footer-main {
+  cursor: pointer;
+  // user-select: "none";
+}
+* {
+  user-select: none;
+}
+</style>
