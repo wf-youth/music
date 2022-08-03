e6223e5a86f742ba46afdc3b0e2b51e05e2fbd88
王锋 <822690543@qq.com>
2022-08-03 15:16:41 +0800

    侧边栏

---
+++ src/components/IconPark/index.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+import { h, resolveDynamicComponent } from "vue";
+import {
+  Github,
+  Star,
+  Broadcast,
+  PlayTwo,
+  Peoples,
+  Download,
+  Time,
+} from "@icon-park/vue-next";
+
+interface Props {
+  name: any;
+}
+
+const icons: any = {
+  Github,
+  Star,
+  Broadcast,
+  PlayTwo,
+  Peoples,
+  Download,
+  Time,
+};
+
+function Icon(props: Props) {
+  const Component: any = resolveDynamicComponent(icons[props.name]);
+  return h(Component);
+}
+export default Icon;
+</script>
--- src/components/Layout/components/layout-sider/index.vue
+++ src/components/Layout/components/layout-sider/index.vue
@@ -3,9 +3,9 @@
     <div class="menus text-sm">
       <div class="menus-item" v-for="item in routes" :key="item.path">
         <div
-          class="cate-name text-xs"
+          class="cate-name text-xs mt-5"
           v-if="item.meta && item.meta.title"
-          :style="{ color: 'var(--color-text-4)' }"
+          :style="{ color: 'var(--color-text-3)' }"
         >
           {{ item.meta.title }}
         </div>
@@ -14,15 +14,18 @@
           :key="child.path"
           :to="child.path"
         >
-          <component
-            v-if="child.meta && child.meta.icon"
-            :is="child.meta.icon"
-            size="24"
-            fill="#333"
-          />
-          <Star theme="outline" size="24" fill="#333" />
-          <div v-if="child.meta && child.meta.title">
-            {{ child.meta.title }}
+          <div class="flex items-center my-2">
+            <icon-park
+              :name="child.meta.icon"
+              v-if="child.meta && child.meta.icon"
+              class="mr-1"
+            />
+            <div
+              v-if="child.meta && child.meta.title"
+              class="cate-name text-sm"
+            >
+              {{ child.meta.title }}
+            </div>
           </div>
         </router-link>
       </div>
@@ -31,10 +34,8 @@
 </template>
 
 <script setup lang="ts">
-import { ref, resolveDynamicComponent } from "vue";
-import { Github, Star } from "@icon-park/vue-next";
 import { routes } from "@/router/modules/index";
-import { useRenderIcon } from "@/components/IconPark/useIconHook";
+import IconPark from "@/components/IconPark/index.vue";
 </script>
 
 <style lang="less" scoped></style>
