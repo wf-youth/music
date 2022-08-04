a53f5c83438cf2fc7f03934f9e0a50f69e34a482
曹贺影 <15155868741@163.com>
2022-08-04 09:54:46 +0800

    优化

--- src/components/Layout/components/layout-footer/index.vue
+++ src/components/Layout/components/layout-footer/index.vue
@@ -10,6 +10,7 @@
         <a-image
           width="50"
           height="50"
+          :preview="false"
           src="https://p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/a8c8cdb109cb051163646151a4a5083b.png~tplv-uwbnlip3yd-webp.webp"
         />
         <!-- 名称 -->
