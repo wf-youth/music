823c19669633e249e346a9b36cb9db06b89b4a73
王锋 <822690543@qq.com>
2022-08-04 13:24:05 +0800

    优化路由

--- src/router/modules/index.ts
+++ src/router/modules/index.ts
@@ -2,19 +2,7 @@ import { RouteRecordRaw } from "vue-router";
 import normalRoutes from "./normal";
 import musicRoutes from "./myMusic";
 
-interface Meta {
-  icon?: string;
-  title: string;
-}
-
-type Routes = RouteRecordRaw & {
-  meta?: Meta;
-  children?: {
-    meta?: Meta;
-  };
-};
-
-export const routes: Routes[] = [
+export const routes: RouteRecordRaw[] = [
   { path: "/", redirect: "/discoverMusic" },
   normalRoutes,
   musicRoutes,
