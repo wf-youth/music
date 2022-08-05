baf17ea6e0a134ecfad25b0d93b742dc51943fdc
王锋 <822690543@qq.com>
2022-08-05 18:04:29 +0800

    推荐歌单

--- src/pages/DiscoverMusic/recommend/components/recommendPlaylist.vue
+++ src/pages/DiscoverMusic/recommend/components/recommendPlaylist.vue
@@ -3,13 +3,9 @@
     <div class="title font-bold text-xl">推荐歌单 <icon-right /></div>
 
     <div class="play-list flex flex-wrap">
-      <div class="play-item mt-6">
-        <img
-          class="play-img rounded-lg mb-2 mr-6"
-          src="http://p1.music.126.net/PzgUTHoKuWyqjuzDAC6S5w==/109951166299954997.jpg?param=140y140"
-          alt=""
-        />
-        <div>国语珍藏 | 给你一张过去的CD</div>
+      <div class="play-item mt-6" v-for="item in playList" :key="item.id">
+        <img class="play-img rounded-lg mb-2 mr-6" :src="item.picUrl" alt="" />
+        <div>{{ item.name }}</div>
       </div>
     </div>
   </div>
@@ -18,7 +14,7 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
 import { usePersonalizedHook } from "../hooks/usePersonalizedHook";
-const { getPlayList } = usePersonalizedHook();
+const { getPlayList, playList } = usePersonalizedHook();
 onMounted(() => {
   getPlayList();
 });
