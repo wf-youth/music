684463adc441d606594e5f1e9a772d2014ed0686
王锋 <822690543@qq.com>
2022-08-05 18:02:10 +0800

    优化

--- src/pages/DiscoverMusic/recommend/hooks/usePersonalizedHook.ts
+++ src/pages/DiscoverMusic/recommend/hooks/usePersonalizedHook.ts
@@ -3,7 +3,7 @@ import { personalized_api } from "@/api/DiscoverMusic/playList";
 import type { PlayList } from "@/type/index";
 
 export const usePersonalizedHook = () => {
-  const playList = ref<PlayList>([]);
+  const playList = ref<PlayList[]>([]);
 
   async function getPlayList() {
     const { result } = await personalized_api();
