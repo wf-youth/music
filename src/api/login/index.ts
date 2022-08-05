e41ea3b11d2321de7fb9001c5e31cd84d1598bc7
曹贺影 <15155868741@163.com>
2022-08-05 17:44:48 +0800

    二维码登录

--- src/api/login/index.ts
+++ src/api/login/index.ts
@@ -1,13 +1,13 @@
 import { get } from '@/utils/http/axios'
-import { QrKeyObject, QrCreate } from './type'
-
+import { QrKeyObject, QrCreate,QrCheck } from './type'
+// 二维码的key
 export function qr_key():Promise<QrKeyObject> {
 
   return get({
     url: `/login/qr/key?timerstamp=${new Date().getTime()}`
   })
 }
-
+// 二维码base64
 export function qr_create(key:string):Promise<QrCreate> {
 
   return get({
@@ -15,4 +15,15 @@ export function qr_create(key:string):Promise<QrCreate> {
     params:{qrimg:true,key}
   })
 }
+
+// 检测二维码状态
+export function qr_check(key:string):Promise<QrCheck> {
+
+  return get({
+    url: `/login/qr/check?timerstamp=${new Date().getTime()}`,
+    params:{key}
+  })
+}
+
+
 
